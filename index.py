ce6c7ab5a947d16a9d5d9e39db4bdcc617858062
Danil Nikolaenko <danil-n@list.ru>
2021-01-29 18:36:39 +0300

    причесал код+ добавил больше правил игнорирования

--- index.py
+++ index.py
@@ -5,19 +5,32 @@ import hashlib
 import os.path
 import pathlib
 
-foldersListToSkan = [
+# список папок, которые надо сканировать
+folders_list_to_skan = [
     "/home/danil/Dropbox",
     "/home/danil/Dropbox (Old)",
     "/home/danil/dropbox_old2",
     "/home/danil/dell_latitude",
 ]
 
-fileExtensionsToIgnore = [
+# игнорировать эти расширения
+file_extensions_to_ignore = [
     'yml',
     'php',
     'go',
     'java',
     'html'
+]
+
+# игнорировать эти папки, абсолютный путь
+ignored_folders_abs = [
+    '/home/danil/dell_latitude/go'
+]
+
+# игнорировать файлы, которые лежат в таких папках
+ignored_folders_rel = [
+    '.git',
+    'go',
 ]
 
 # вычисление md5 на больших файлах будет делаться медленно, поэтому скипаем их
@@ -36,62 +49,80 @@ def md5(fname: str):
     return hash_md5.hexdigest()
 
 
-def makeFileName(dirpath: str, filename: str):
+def make_file_name(dirpath: str, filename: str):
     return dirpath + "/" + filename
 
 
-def convertHumanReadable(size_in_bytes):
+def convert_human_readable(size_in_bytes):
     return size_in_bytes / (1024 * 1024)
 
 
-def getFileSize(file_name):
+def get_file_size(file_name):
     size = os.path.getsize(file_name)
-    return convertHumanReadable(size)
+    return convert_human_readable(size)
 
 
 files = {}
 
 
-def fileExists(fileName):
+def file_exists(fileName):
     return os.path.isfile(fileName)
 
 
-def fileExtensionIgnored(file: str):
-    return pathlib.Path(file).suffix in fileExtensionsToIgnore
+def file_extension_ignored(file: str):
+    return pathlib.Path(file).suffix in file_extensions_to_ignore
+
+
+def path_ignored(path: str):
+    return path in ignored_folders_abs
+
+
+def folder_ignored(path: str) -> bool:
+    return os.path.basename(path) in ignored_folders_rel
+
+
+def folder_name_ignored(path: str) -> bool:
+    print(os.path.dirname(path))
+    return False
 
 
 def scan(dirName: str, files: dict):
     for (dirpath, dirnames, filenames) in walk(dirName):
+        if path_ignored(dirpath):
+            continue
+        if folder_ignored(dirpath):
+            continue
         for file in filenames:
-            if fileExtensionIgnored(file):
-                continue
-            fullFileName = makeFileName(dirpath, file)
-            if not fileExists(fullFileName):
-                print("can't handle file {filename} ".format(filename=fullFileName))
+            if file_extension_ignored(file):
                 continue
 
-            fileSize = getFileSize(fullFileName)
-            if int(fileSize) > MAX_FILE_SIZE_TO_CHECK_IN_MB:
-                print("file {filename} is bigger than {maxsize}, skip...".format(filename=fullFileName,
+            full_file_name = make_file_name(dirpath, file)
+            if not file_exists(full_file_name):
+                print("can't handle file {filename} ".format(filename=full_file_name))
+                continue
+
+            file_size = get_file_size(full_file_name)
+            if int(file_size) > MAX_FILE_SIZE_TO_CHECK_IN_MB:
+                print("file {filename} is bigger than {maxsize}, skip...".format(filename=full_file_name,
                                                                                  maxsize=MAX_FILE_SIZE_TO_CHECK_IN_MB))
                 continue
-            key = md5(makeFileName(dirpath, file))
-            existingFile = files.get(key)
-            if existingFile is None:
-                files[key] = {COUNT: 1, SIZE: fileSize, FILENAMES: [fullFileName]}
+            key = md5(make_file_name(dirpath, file))
+            existing_file = files.get(key)
+            if existing_file is None:
+                files[key] = {COUNT: 1, SIZE: file_size, FILENAMES: [full_file_name]}
             else:
-                existingFile[COUNT] = existingFile[COUNT] + 1
-                existingFile[FILENAMES].append(fullFileName)
-                files[key] = existingFile
+                existing_file[COUNT] = existing_file[COUNT] + 1
+                existing_file[FILENAMES].append(full_file_name)
+                files[key] = existing_file
 
 
-for folderToScan in foldersListToSkan:
-    scan(folderToScan, files)
+for folder_to_scan in folders_list_to_skan:
+    scan(folder_to_scan, files)
 
-for fileKey in files:
-    filedata = files.get(fileKey)
-    if filedata[COUNT] > 1:
-        print("count: {count}, filesize in MB:{filesize}, filepaths:".format(count=filedata[COUNT],
-                                                                             filesize="{:.2f}".format(filedata[SIZE])))
-        for duplicatePath in filedata[FILENAMES]:
-            print(" " * 2 + duplicatePath)
+for file_key in files:
+    file_data = files.get(file_key)
+    if file_data[COUNT] > 1:
+        print("count: {count}, filesize in MB:{filesize}, filepaths:".format(count=file_data[COUNT],
+                                                                             filesize="{:.2f}".format(file_data[SIZE])))
+        for duplicate_path in file_data[FILENAMES]:
+            print(" " * 2 + duplicate_path)
