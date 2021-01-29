b29ab69d0b71f4839d845a82f94db77522e08f24
Danil Nikolaenko <danil-n@list.ru>
2021-01-29 17:42:39 +0300

    ext blacklist and folders list support

--- index.py
+++ index.py
@@ -3,16 +3,30 @@ from os import walk
 import os
 import hashlib
 import os.path
+import pathlib
 
-FOLDER_NAME_TO_SEARCH = "/home/danil/test_folder"
+foldersListToSkan = [
+    "/home/danil/Dropbox",
+    "/home/danil/Dropbox (Old)",
+    "/home/danil/dropbox_old2",
+    "/home/danil/dell_latitude",
+]
+
+fileExtensionsToIgnore = [
+    'yml',
+    'php',
+    'go',
+    'java',
+    'html'
+]
+
 # вычисление md5 на больших файлах будет делаться медленно, поэтому скипаем их
-MAX_FILE_SIZE_TO_CHECK_IN_MB = 100
-
-
+MAX_FILE_SIZE_TO_CHECK_IN_MB = 1000
 
 FILENAMES = "filenames"
 COUNT = "count"
 SIZE = "size"
+
 
 def md5(fname: str):
     hash_md5 = hashlib.md5()
@@ -42,26 +56,37 @@ def fileExists(fileName):
     return os.path.isfile(fileName)
 
 
-for (dirpath, dirnames, filenames) in walk(FOLDER_NAME_TO_SEARCH):
-    for file in filenames:
-        fullFileName = dirpath + "/" + file
-        if not fileExists(fullFileName):
-            print("can't handle file {filename} ".format(filename=fullFileName))
-            continue
+def fileExtensionIgnored(file: str):
+    return pathlib.Path(file).suffix in fileExtensionsToIgnore
 
-        fileSize = getFileSize(fullFileName)
-        if int(fileSize) > MAX_FILE_SIZE_TO_CHECK_IN_MB:
-            print("file {filename} is bigger than {maxsize}, skip...".format(filename=fullFileName,
-                                                                             maxsize=MAX_FILE_SIZE_TO_CHECK_IN_MB))
-            continue
-        key = md5(makeFileName(dirpath, file))
-        existingFile = files.get(key)
-        if existingFile is None:
-            files[key] = {COUNT: 1, SIZE: fileSize, FILENAMES: [fullFileName]}
-        else:
-            existingFile[COUNT] = existingFile[COUNT] + 1
-            existingFile[FILENAMES].append(fullFileName)
-            files[key] = existingFile
+
+def scan(dirName: str, files: dict):
+    for (dirpath, dirnames, filenames) in walk(dirName):
+        for file in filenames:
+            if fileExtensionIgnored(file):
+                continue
+            fullFileName = makeFileName(dirpath, file)
+            if not fileExists(fullFileName):
+                print("can't handle file {filename} ".format(filename=fullFileName))
+                continue
+
+            fileSize = getFileSize(fullFileName)
+            if int(fileSize) > MAX_FILE_SIZE_TO_CHECK_IN_MB:
+                print("file {filename} is bigger than {maxsize}, skip...".format(filename=fullFileName,
+                                                                                 maxsize=MAX_FILE_SIZE_TO_CHECK_IN_MB))
+                continue
+            key = md5(makeFileName(dirpath, file))
+            existingFile = files.get(key)
+            if existingFile is None:
+                files[key] = {COUNT: 1, SIZE: fileSize, FILENAMES: [fullFileName]}
+            else:
+                existingFile[COUNT] = existingFile[COUNT] + 1
+                existingFile[FILENAMES].append(fullFileName)
+                files[key] = existingFile
+
+
+for folderToScan in foldersListToSkan:
+    scan(folderToScan, files)
 
 for fileKey in files:
     filedata = files.get(fileKey)
