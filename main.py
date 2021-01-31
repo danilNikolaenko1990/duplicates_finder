e67ffaff7116cb61ce5b68490ff2ef6573b6a6ae
Danil Nikolaenko <danil-n@list.ru>
2021-01-31 14:26:41 +0300

    reformat and codestyle

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ print("-------report---------")
 for file_key in f.files:
     file_data = f.files.get(file_key)
     if file_data[f.COUNT] > 1:
-        print("count: {count}, filesize in MB:{filesize}, filepaths:".format(count=file_data[COUNT],
-                                                                             filesize="{:.2f}".format(file_data[SIZE])))
+        print("count: {count}, filesize in MB:{filesize}, filepaths:".format(count=file_data[f.COUNT],
+                                                                             filesize="{:.2f}".format(
+                                                                                 file_data[f.SIZE])))
         for duplicate_path in file_data[f.FILENAMES]:
             print(" " * 2 + duplicate_path)
 
